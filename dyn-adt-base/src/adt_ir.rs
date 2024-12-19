86e09c9ee3d8146869f19ce81ae43c7bbd4d618b
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-12-19 23:34:06 +0900

    chore: Change alternative index to u64

--- dyn-adt-base/src/adt_ir.rs
+++ dyn-adt-base/src/adt_ir.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::primitive_type::PrimitiveType;
 
 // Intermediate representation of ADT values
+// Field and constructor names are not included
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum AdtIr {
     Primitive(PrimitiveType),
